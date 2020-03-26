778ed0c2a7a3ac0f07adc74a25262e7ecc2d860a
alexander.holmstro3 <alle020916@gmail.com>
2020-03-26 11:18:22 +0100

    Initial commit

---
+++ src/Sprite2.java
@@ -0,0 +1,26 @@
+public class Sprite2 {
+    private int width;
+    private int height;
+    private int[] pixels;
+
+    public Sprite(int w, int h) {
+        this.width = w;
+        this.height = h;
+        pixels = new int[w*h];
+        for (int i = 0 ; i < pixels.length ; i++) {
+            pixels[i] = 0xFF00FF;
+        }
+    }
+
+    public int[] getPixels() {
+        return pixels;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
